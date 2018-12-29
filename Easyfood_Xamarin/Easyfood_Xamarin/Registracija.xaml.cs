2f1f9b3c6f86ef83d1890119fb45428da4b2b71f
Eldar Jahijagić <eldar.jahijagic@edu.fit.ba>
2018-12-29 15:38:26 +0100

    Initial xamarin shared project.

---
+++ Easyfood_Xamarin/Easyfood_Xamarin/Registracija.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Easyfood_Xamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registracija : ContentPage
+	{
+		public Registracija ()
+		{
+			InitializeComponent ();
+            this.Title = "Registracija";
+        }
+	}
+}
